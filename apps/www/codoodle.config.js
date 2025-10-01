f73d98ea983bd33f066d8fb048f3bf9d16f785d0
Yeongseok Yoon <zerostone@live.com>
2025-10-01 16:51:01 +0900

    chore: add codoodle configuration for MDX content processing

---
+++ apps/www/codoodle.config.js
@@ -0,0 +1,96 @@
+/**
+ * @type {import("@codoodle/mdx").MdxConfig}
+ */
+export default {
+  content: {
+    "blog/index.md{,x}": {
+      name: "Blog",
+      output: "blog",
+      frontmatter: {
+        name: {
+          type: "string",
+          required: true,
+        },
+        description: {
+          type: "string",
+        },
+      },
+    },
+    "blog/**/index.md{,x}": {
+      name: "Category",
+      output: "blog/categories",
+      frontmatter: {
+        name: {
+          type: "string",
+          required: true,
+        },
+        description: {
+          type: "string",
+        },
+      },
+    },
+    "blog/**/*.md{,x}": {
+      name: "Post",
+      output: "blog/posts",
+      frontmatter: {
+        title: {
+          type: "string",
+          required: true,
+        },
+        description: {
+          type: "string",
+        },
+        datePublished: {
+          type: "date",
+          required: true,
+        },
+        dateModified: {
+          type: "date",
+        },
+        author: {
+          type: "string",
+        },
+        tags: {
+          type: "array",
+          fields: {
+            slug: {
+              type: "string",
+              required: true,
+            },
+            name: {
+              type: "string",
+              required: true,
+            },
+          },
+        },
+        prev: {
+          type: "object",
+          fields: {
+            slug: {
+              type: "string",
+              required: true,
+            },
+            title: {
+              type: "string",
+            },
+          },
+        },
+        next: {
+          type: "object",
+          fields: {
+            slug: {
+              type: "string",
+              required: true,
+            },
+            title: {
+              type: "string",
+            },
+          },
+        },
+      },
+    },
+  },
+  contentCompileOptions: {
+    jsx: true,
+  },
+};
